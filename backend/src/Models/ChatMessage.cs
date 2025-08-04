39dec51459245dd2e9f6f6e38c78097eefe7adb0
tiagogcastro <tiaguin180@gmail.com>
2025-08-04 08:57:58 -0300

    refactor: removed import not used

--- backend/src/Models/ChatMessage.cs
+++ backend/src/Models/ChatMessage.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Text.Json.Serialization;
 
 namespace HighCapital.Chatbot.Api.Models
--- backend/src/Services/ChatService.cs
+++ backend/src/Services/ChatService.cs
@@ -1,6 +1,4 @@
 using OpenAI_API;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace HighCapital.Chatbot.Api.Services
 {
